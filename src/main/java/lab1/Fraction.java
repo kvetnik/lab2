50786a594b433ceaabdb0026d283ce4c879f12fa
Никита <nikita@MacBook-Air-Nikita-2.local>
2019-12-21 12:33:40 +0300

    release v0.1

--- src/main/java/lab1/Fraction.java
+++ src/main/java/lab1/Fraction.java
@@ -3,7 +3,7 @@ package lab1;
 public class Fraction {
     private int n,d;
     public Fraction(){}
-    //лаба1
+    //f
     /** 
      * Create a new Fraction with specified numeration and denomimator
      * @param n
